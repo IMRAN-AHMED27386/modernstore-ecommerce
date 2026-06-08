6b3b7e1c6a1c13a6808abf7533eff72eca17b7d9
IMRAN AHAMAD <imran27386@gmail.com>
2026-06-09 03:50:55 +0530

    Change base path for Vite configuration
    Repository name changed...

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/E-Commerce-Dubai/',
+  base: '/modernstore-ecommerce/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
